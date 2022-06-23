a4f4a1529dda081074e9384afc4d0973badd150e
ErickGuillen17 <80604246+ErickGuillen17@users.noreply.github.com>
2022-06-23 12:13:07 -0600

    Clase 4 completa

--- app/src/main/java/com/lugares/ui/lugar/LugarFragment.kt
+++ app/src/main/java/com/lugares/ui/lugar/LugarFragment.kt
@@ -4,13 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.lugares.R
+import com.lugares.adapter.LugarAdapter
 import com.lugares.databinding.FragmentAddLugarBinding
 import com.lugares.databinding.FragmentLugarBinding
+import com.lugares.model.Lugar
 import com.lugares.viewmodel.LugarViewModel
 
 class LugarFragment : Fragment() {
@@ -30,6 +34,19 @@ class LugarFragment : Fragment() {
         //Se programa la acción para pasarse a AddLugar
         binding.addLugarButton.setOnClickListener {
             findNavController().navigate(R.id.action_nav_lugar_to_addLugarFragment)
+        }
+
+        //Activar el Reciclador -RecyclerView
+        val lugarAdapter = LugarAdapter()
+        val reciclador = binding.reciclador
+
+        reciclador.adapter = lugarAdapter
+        reciclador.layoutManager = LinearLayoutManager(requireContext())
+
+        lugarViewModel = ViewModelProvider(this)[LugarViewModel::class.java]
+
+        lugarViewModel.getAllData.observe(viewLifecycleOwner) {
+            lugares -> lugarAdapter.setData(lugares)
         }
 
         return binding.root
